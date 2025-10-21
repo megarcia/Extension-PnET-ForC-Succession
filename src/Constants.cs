0e0479f4f3deeefa5db85c48cbfea66ef64e5eaa
Matthew Garcia <matt.e.garcia@gmail.com>
2025-10-21 14:18:59 -0500

    merged PnET and ForC Constants; seems like some of the ForC constants should perhaps become input variables instead of having hard-coded values

--- src/Constants.cs
+++ src/Constants.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿// merged PnET and ForC Constants
+
+using System;
 
 namespace Landis.Extension.Succession.PnETForC 
 {
@@ -207,5 +209,24 @@ namespace Landis.Extension.Succession.PnETForC
         /// convert kJ/m.d.K to W/m.K
         /// </summary>
         public const float Convert_kJperday_to_Watts = 0.2777777778F / 24F;
+
+        /// <summary>
+        /// ForC Constants
+        /// Shouldn't some of these be input variables instead? 
+        /// </summary>
+        public const int FIREINTENSITYCOUNT = 5;
+        public const int NUMSNAGS = 1000;
+        public const int NUMBIOMASSCOMPONENTS = 6;  // ComponentType.FINEROOT + 1, The total number of biomass components.
+        public const double BIOTOC = 0.5;
+        public const int NUMSOILPOOLS = 10; // SoilPoolType.BLACKCARBON + 1;
+        public const int NUMSNAGPOOLS = 2; // Snags.SnagType.OTHERSNAG + 1 i.e., stem and branches snag pool
+        public const double FINEROOTSABOVERATIO = 0.5;
+        public const double COARSEROOTABOVERATIO = 0.5;
+        public const int NUMDISTURBANCES = 9;  // note, if adding more disturbances, then increase this
+        public const double DECAYREFTEMP = 10.0;  // reference soil temperature for decay calculation; originally decayRefTemp from SoilDecay.CalcDecayFTemp
+        public const int NUMSLOWPOOLS = 2;  // number of above- and below-ground slow soil carbon pool; originally numberABSlowPool from Soils.DoSoilDynamics
+        public const int AGSLOWPOOLIDX = 0;  // above-ground slow carbon pool; originally aboveSlowPool from Soils.DoSoilDynamics
+        public const int BGSLOWPOOLIDX = 1;  // below-ground slow soil carbon pool; originally belowSlowPool from Soils.DoSoilDynamics
+
     }
 }
