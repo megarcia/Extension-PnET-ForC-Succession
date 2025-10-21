a77d45f1b9f64f70bcba4ed66698562473f89250
Matthew Garcia <matt.e.garcia@gmail.com>
2025-10-21 15:20:27 -0500

    added some explanation in CrownScorching function; will likely move this to a revised Disturbance class

--- src/FireEffects_ForC.cs
+++ src/FireEffects_ForC.cs
@@ -9,23 +9,26 @@ namespace Landis.Extension.Succession.PnETForC
     public class FireEffects
     {
         /// <summary>
-        /// Crown scorching, when a cohort loses its foliage but is not killed.
+        /// Crown scorching is when a cohort loses its foliage to fire  
+        /// but is not killed. If cohort species is capable of epicormic 
+        /// sprouting, returns the likelihood of sprouting after the fire 
+        /// based on fire severity and remaining species longevity. 
         /// </summary>
         /// <param name="cohort"></param>
-        /// <param name="siteSeverity"></param>
+        /// <param name="fireSeverity"></param>
         /// <returns></returns>
-        public static double CrownScorching(ICohort cohort, byte siteSeverity)
+        public static double CrownScorching(ICohort cohort, byte fireSeverity)
         {
-            int difference = (int)siteSeverity - SpeciesData.FireTolerance[cohort.Species];
-            double ageFraction = 1.0 - ((double)cohort.Data.Age / (double)cohort.Species.Longevity);
+            int diff = (int)fireSeverity - SpeciesData.FireTolerance[cohort.Species];
+            double ageFrac = 1.0 - ((double)cohort.Data.Age / (double)cohort.Species.Longevity);
             if (SpeciesData.Epicormic[cohort.Species])
             {
-                if (difference < 0)
-                    return 0.5 * ageFraction;
-                if (difference == 0)
-                    return 0.75 * ageFraction;
-                if (difference > 0)
-                    return 1.0 * ageFraction;
+                if (diff < 0)  // fire severity < fire tolerance
+                    return 0.5 * ageFrac;
+                if (diff == 0)  // fire severity = fire tolerance
+                    return 0.75 * ageFrac;
+                if (diff > 0)  // fire severity > fire tolerance
+                    return 1.0 * ageFrac;
             }
             return 0.0;
         }
