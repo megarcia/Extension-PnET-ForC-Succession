32acbfe323412b48da9ef2168d42118879797c96
Matthew Garcia <matt.e.garcia@gmail.com>
2026-01-12 11:46:30 -0600

    converted float-type variables to double type

--- src/ICohort_PnET.cs
+++ src/ICohort_PnET.cs
@@ -13,42 +13,42 @@ namespace Landis.Extension.Succession.PnETForC
         int AGBiomass { get; }
         int TotalBiomass { get; }
         bool IsLeafOn { get; }
-        float MaxBiomass { get; }
-        float Fol { get; }
-        float MaxFolYear { get; }
-        float NSC { get; }
-        float DefoliationFrac { get; }
-        float LastWoodSenescence { get; }
-        float LastFolSenescence { get; }
-        float LastFRad { get; }
-        List<float> LastSeasonFRad { get; }
-        float adjFolBiomassFrac { get; }
-        float AdjHalfSat { get; }
-        float adjFolN { get; }
+        double MaxBiomass { get; }
+        double Fol { get; }
+        double MaxFolYear { get; }
+        double NSC { get; }
+        double DefoliationFrac { get; }
+        double LastWoodSenescence { get; }
+        double LastFolSenescence { get; }
+        double LastFRad { get; }
+        List<double> LastSeasonFRad { get; }
+        double adjFolBiomassFrac { get; }
+        double AdjHalfSat { get; }
+        double adjFolN { get; }
         int ColdKill { get; }
         byte Layer { get; }
-        float[] LAI { get; }
-        float LastLAI { get; }
-        float LastAGBio { get; }
-        float[] GrossPsn { get; }
-        float[] FoliarRespiration { get; }
-        float[] NetPsn { get; }
-        float[] MaintenanceRespiration { get; }
-        float[] Transpiration { get; }
-        float[] PotentialTranspiration { get; }
-        float[] FRad { get; }
-        float[] FWater { get; }
-        float[] SoilWaterContent { get; }
-        float[] PressHead { get; }
+        double[] LAI { get; }
+        double LastLAI { get; }
+        double LastAGBio { get; }
+        double[] GrossPsn { get; }
+        double[] FoliarRespiration { get; }
+        double[] NetPsn { get; }
+        double[] MaintenanceRespiration { get; }
+        double[] Transpiration { get; }
+        double[] PotentialTranspiration { get; }
+        double[] FRad { get; }
+        double[] FWater { get; }
+        double[] SoilWaterContent { get; }
+        double[] PressHead { get; }
         int[] NumPrecipEvents { get; }
-        float[] FOzone { get; }
-        float[] Interception { get; }
-        float[] AdjFolN { get; }
-        float[] AdjFolBiomassFrac { get; }
-        float[] CiModifier { get; }
-        float[] DelAmax { get; }
-        float BiomassLayerFrac { get; }
-        float CanopyLayerFrac { get; }
-        float CanopyGrowingSpace { get; }
+        double[] FOzone { get; }
+        double[] Interception { get; }
+        double[] AdjFolN { get; }
+        double[] AdjFolBiomassFrac { get; }
+        double[] CiModifier { get; }
+        double[] DelAmax { get; }
+        double BiomassLayerFrac { get; }
+        double CanopyLayerFrac { get; }
+        double CanopyGrowingSpace { get; }
     }
 }
--- src/ISiteCohorts_PnET.cs
+++ src/ISiteCohorts_PnET.cs
@@ -9,26 +9,26 @@ namespace Landis.Extension.Succession.PnETForC
 {
     public interface ISiteCohorts : Library.UniversalCohorts.ISiteCohorts
     {
-        float[] NetPsn { get; }
-        float[] MaintResp { get; }
-        float[] GrossPsn { get; }
-        float[] FoliarRespiration { get; }
-        float[] AverageAlbedo { get; }
-        float[] ActiveLayerDepth { get; }
-        float[] FrostDepth { get; }
-        float[] MonthlyAvgSnowpack { get; }
-        float[] MonthlyAvgWater { get; }
-        float[] MonthlyAvgLAI { get; }
-        float[] MonthlyEvap { get; }
-        float[] MonthlyActualTrans { get; }
-        float[] MonthlyLeakage { get; }
-        float[] MonthlyInterception { get; }
-        float[] MonthlyRunoff { get; }
-        float[] MonthlyActualET { get; }
-        float[] MonthlyPotentialEvap { get; }
-        float[] MonthlyPotentialTrans { get; }
-        float CanopyLAImax { get; }
-        float SiteMossDepth { get; }
+        double[] NetPsn { get; }
+        double[] MaintResp { get; }
+        double[] GrossPsn { get; }
+        double[] FoliarRespiration { get; }
+        double[] AverageAlbedo { get; }
+        double[] ActiveLayerDepth { get; }
+        double[] FrostDepth { get; }
+        double[] MonthlyAvgSnowpack { get; }
+        double[] MonthlyAvgWater { get; }
+        double[] MonthlyAvgLAI { get; }
+        double[] MonthlyEvap { get; }
+        double[] MonthlyActualTrans { get; }
+        double[] MonthlyLeakage { get; }
+        double[] MonthlyInterception { get; }
+        double[] MonthlyRunoff { get; }
+        double[] MonthlyActualET { get; }
+        double[] MonthlyPotentialEvap { get; }
+        double[] MonthlyPotentialTrans { get; }
+        double CanopyLAImax { get; }
+        double SiteMossDepth { get; }
         int AverageAge { get; }
         Library.Parameters.Species.AuxParm<int> CohortCountPerSpecies { get; }
         Library.Parameters.Species.AuxParm<bool> SpeciesPresent { get; }
@@ -40,28 +40,28 @@ namespace Landis.Extension.Succession.PnETForC
         Library.Parameters.Species.AuxParm<int> BGBiomassPerSpecies { get; }
         Library.Parameters.Species.AuxParm<int> FoliageBiomassPerSpecies { get; }
         Library.Parameters.Species.AuxParm<int> NSCPerSpecies { get; }
-        Library.Parameters.Species.AuxParm<float> LAIPerSpecies { get; }
+        Library.Parameters.Species.AuxParm<double> LAIPerSpecies { get; }
         Library.Parameters.Species.AuxParm<int> WoodSenescencePerSpecies { get; }
         Library.Parameters.Species.AuxParm<int> FolSenescencePerSpecies { get; }
         Library.Parameters.Species.AuxParm<List<ushort>> CohortAges { get; }
-        float BiomassSum { get; }
-        float AGBiomassSum { get; }
-        float WoodBiomassSum { get; }
-        float WoodSenescenceSum { get; }
-        float FolSenescenceSum { get; }
+        double BiomassSum { get; }
+        double AGBiomassSum { get; }
+        double WoodBiomassSum { get; }
+        double WoodSenescenceSum { get; }
+        double FolSenescenceSum { get; }
         int CohortCount { get; }
-        float JulySubCanopyPAR { get; }
-        float MaxSubCanopyPAR { get; }
+        double JulySubCanopyPAR { get; }
+        double MaxSubCanopyPAR { get; }
         double LeafLitter { get; }
         double WoodDebris { get; }
         int AgeMax { get; }
-        float AvgSoilWaterContent { get; }
-        float BGBiomassSum { get; }
-        float FolBiomassSum { get; }
-        float NSCSum { get; }
-        float ActualETSum { get; } //mm
-        float NetPsnSum { get; }
-        float PotentialET { get; }
+        double AvgSoilWaterContent { get; }
+        double BGBiomassSum { get; }
+        double FolBiomassSum { get; }
+        double NSCSum { get; }
+        double ActualETSum { get; } //mm
+        double NetPsnSum { get; }
+        double PotentialET { get; }
 
         List<ISpecies> SpeciesEstByPlanting { get; set; }
         List<ISpecies> SpeciesEstBySerotiny { get; set; }
